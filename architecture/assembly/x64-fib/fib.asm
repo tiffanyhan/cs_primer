1fa099f4966cf7897264ff311816bb523abe68c9
Tiffany Han <tiffany@freshpaint.io>
2025-05-11 16:51:05 -0700

    update fib

--- architecture/assembly/x64-fib/fib.asm
+++ architecture/assembly/x64-fib/fib.asm
@@ -3,23 +3,21 @@ global fib
 fib:
 	; rdi: n
 
-	cmp rdi, 0x00
-	je _return_0
+	cmp rdi, 0x01 ; if n is 0 or 1, return n
+	mov rax, rdi 
+	jle _done
 
-	cmp rdi, 0x02
-	jle _return_1
-
-	push rdi ; save n before decrementing for recursive calls
 	dec rdi ;
+	push rdi ; save n-1 before recursive calls clobber it
 
 	call fib ; fib(n-1)
 
-	pop rdi ; get n
+	pop rdi ; get n-1
 	push rax ; stack now has fib(n-1)
 
 	; fib(n-1) has now returned
 
-	sub rdi, 2 ; rdi: n-2
+	dec rdi ; rdi: n-2
 
 	call fib ; fib(n-2)
 	mov rsi, rax ; rsi: fib(n-2)
@@ -27,12 +25,5 @@ fib:
 	pop rax ; rax: fib(n-1) 
 	add rax, rsi ; rax: fib(n-1) + fib(n-2)
 	
-	ret
-
-_return_0:
-	mov rax, 0x00
-	ret
-
-_return_1:
-	mov rax, 0x01
+_done:
 	ret
