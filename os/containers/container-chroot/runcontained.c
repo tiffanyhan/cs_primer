49a4ae651f4d8708ff07845dfcfffd154ce13c63
Tiffany Han <tiffany@freshpaint.io>
2025-06-29 17:28:46 -0700

    completed chroot

---
+++ os/containers/container-chroot/runcontained.c
@@ -0,0 +1,58 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define STACK_SIZE 65536
+
+struct child_config {
+  int argc;
+  char **argv;
+};
+
+/* Entry point for child after `clone` */
+int child(void *arg) {
+  if (chroot("/new_root") != 0) {
+    printf("chroot error code: %d\n", errno);
+  };
+  struct child_config *config = arg;
+  char *envp[] = { "PATH=/bin:/usr/bin", NULL };
+
+  if (execvpe(config->argv[0], config->argv, envp)) {
+    fprintf(stderr, "execvpe failed %m.\n");
+    return -1;
+  }
+  return 0;
+}
+
+
+int main(int argc, char**argv) {
+  struct child_config config = {0};
+  int flags = 0;
+  pid_t child_pid = 0;
+
+  // Prepare child configuration,
+  // command will be run as sudo
+  config.argc = argc - 1;
+  config.argv = &argv[1];
+
+  // Allocate stack for child
+  char *stack = 0;
+  if (!(stack = malloc(STACK_SIZE))) {
+    fprintf(stderr, "Malloc failed");
+    exit(1);
+  }
+
+  // Clone parent, enter child code
+  if ((child_pid = clone(child, stack + STACK_SIZE, flags | SIGCHLD, &config)) == -1) {
+    fprintf(stderr, "Clone failed");
+    exit(2);
+  }
+
+  wait(NULL);
+  
+  return 0;
+}
